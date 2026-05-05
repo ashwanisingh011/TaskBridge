ec95f5ffe79249a40cac691c01e56eb8c3635399
ashwanisingh011 <ashwanisingh9737@gmail.com>
2026-05-05 18:58:14 +0530

    next js frontend

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/useAuth';
-
-const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
